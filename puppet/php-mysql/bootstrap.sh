4788c6f41b17c2d1233b30ae0fce3c23dc6e8bd9
Stéphane Albert <sheeprine@oh.its.fake.nullplace.com>
2014-04-30 11:22:14 +0200

    Changed version of nodes-php
    The version from the forge doesn't work for our deployment, we're using
    the git version instead.

--- puppet/php-mysql/bootstrap.sh
+++ puppet/php-mysql/bootstrap.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
 puppet module install jfryman/nginx
-puppet module install nodes/php
+# Problem with recent PHP versions, getting it from git
+# puppet module install nodes/php
+apt-get install -y git
+git clone https://github.com/jippi/puppet-php.git /etc/puppet/modules/php
 puppet module install puppetlabs-mysql
